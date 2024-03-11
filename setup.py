b76c1d883fca8c94c79facc07247ce1b184ddbfc
TabeaSonnenschein <tabea.sonnenschein@gmail.com>
2024-03-11 15:02:32 +0100

    x

--- setup.py
+++ setup.py
@@ -4,11 +4,29 @@ from setuptools import setup, find_packages
 # Example setup.py
 
 setup(
-    name='mypackage',
+    name='CellAutDisp',
     version='0.1',
     packages=find_packages(),
     install_requires=[
         # List your dependencies here
+        'numpy',
+        'pandas',
+        'matplotlib',
+        'seaborn',
+        'os',
+        'time',
+        'joblib',
+        'math',
+        'geneticalgorithm2',
+        'datetime',
+        'json',
+        'xarray-spatial',
+        'geopandas',
+        'shapely',
+        'scipy',
+        'xarray',
+        'sklearn'
+
     ],
     entry_points={
         'console_scripts': [
