0e96cccaf40971fda8c0b95dd0913b8df20304ff
TabeaSonnenschein <tabea.sonnenschein@gmail.com>
2024-03-18 13:01:44 +0100

    x

--- CellAutDisp/data_preparation.py
+++ CellAutDisp/data_preparation.py
@@ -35,14 +35,14 @@ def FindRoadNeighboringCells(raster, onroadindices):
 
 
 
-def create_traffic_emission_columns(df, traff_coeff=np.nan, TrI_coeff=np.nan, traffroadlength_coeff=np.nan, suffix=""):
+def create_traffic_emission_columns(df, id, TrV_coeff=np.nan, TrI_coeff=np.nan, suffix=""):
     """Calculate traffic-related NO2 emissions based on traffic LUR coefficients and traffic data and the baseline NO2
     and produce the Predictor Dataframe (Pred_df) accordingly. The function also saves the Pred_df with the indicated suffix to a csv file.
 
     Parameters:
     - df (pd.DataFrame): Input DataFrame containing relevant columns.
-    - traff_coeff (float, optional): Coefficient for traffic volume. Default is NaN.
-    - traffroadlength_coeff (float, optional): Coefficient for the interaction between traffic volume and road length. Default is NaN.
+    - id (str): Name of the column containing the unique identifier for each row.
+    - TrV_coeff (float, optional): Coefficient for traffic volume. Default is NaN.
     - TrI_coeff (float, optional): Coefficient for traffic intensity. Default is NaN.
 
     Returns:
@@ -56,15 +56,15 @@ def create_traffic_emission_columns(df, traff_coeff=np.nan, TrI_coeff=np.nan, tr
     - Columns assumed in the DataFrame: 'road_length', 'baseline_NO2', 'ON_ROAD', and hourly traffic volumes.
 
     Example:
-    >>> df = pd.DataFrame({'TrV0_1': [10, 20, 30], 'road_length': [50, 60, 70], 'baseline_NO2': [5, 5, 5], 'ON_ROAD': [1, 0, 1]})
-    >>> updated_df = create_traffic_emission_columns(df, traff_coeff=2, traffroadlength_coeff=0.5)
+    >>> df = pd.DataFrame({'int_id': [1,2,3], 'TrV0_1': [10, 20, 30],  'baseline_NO2': [5, 5, 5], 'ON_ROAD': [1, 0, 1]})
+    >>> updated_df = create_traffic_emission_columns(df, id= "int_id", TrV_coeff=2, TrI_coeff=0.5)
     """
     traffVolhours = ["TrV0_1","TrV1_2","TrV2_3","TrV3_4","TrV4_5","TrV5_6",
                      "TrV6_7","TrV7_8","TrV8_9","TrV9_10","TrV10_11","TrV11_12", 
                      "TrV12_13","TrV13_14","TrV14_15","TrV15_16","TrV16_17","TrV17_18",  
                      "TrV18_19","TrV19_20","TrV20_21","TrV21_22","TrV22_23","TrV23_24"]
 
-    traffIntsenshours = ["TrI0_1","TrI1_2","TrI2_3","TrI3_4","TrI4_5","TrI5_6",
+    traffIntenshours = ["TrI0_1","TrI1_2","TrI2_3","TrI3_4","TrI4_5","TrI5_6",
                          "TrI6_7","TrI7_8","TrI8_9","TrI9_10","TrI10_11","TrI11_12", 
                          "TrI12_13","TrI13_14","TrI14_15","TrI15_16","TrI16_17","TrI17_18",  
                          "TrI18_19","TrI19_20","TrI20_21","TrI21_22","TrI22_23","TrI23_24"]
@@ -84,24 +84,22 @@ def create_traffic_emission_columns(df, traff_coeff=np.nan, TrI_coeff=np.nan, tr
                           "NO2_16_17_basetraff","NO2_17_18_basetraff", "NO2_18_19_basetraff", "NO2_19_20_basetraff","NO2_20_21_basetraff","NO2_21_22_basetraff", 
                           "NO2_22_23_basetraff", "NO2_23_24_basetraff"]
 
-    if np.isnan(traffroadlength_coeff) and np.isnan(TrI_coeff) and not np.isnan(traff_coeff):
-        print("only Traffic Volume without roadlength interaction")
-        df[TrafficNO2_nobaseline] = df[traffVolhours] * traff_coeff
-    elif np.isnan(TrI_coeff) and not np.isnan(traff_coeff) and not np.isnan(traffroadlength_coeff):
-        print("Traffic volume with roadlength interaction")
-        df[TrafficNO2_nobaseline] = (df[traffVolhours] * traff_coeff) + (df['road_length'] * df[traffVolhours] * traffroadlength_coeff)
-    elif not np.isnan(TrI_coeff) and not np.isnan(traff_coeff) and np.isnan(traffroadlength_coeff):
+    if  np.isnan(TrI_coeff) and not np.isnan(TrV_coeff):
+        print("only Traffic Volume without traffic intensity")
+        df[TrafficNO2_nobaseline] = df[traffVolhours] * TrV_coeff
+    elif not np.isnan(TrI_coeff) and not np.isnan(TrV_coeff):
         print("with traffic intensity and traffic volume")
-        df[TrafficNO2_nobaseline] = (df[traffVolhours] * traff_coeff) + (df[traffIntsenshours] * TrI_coeff)
-    elif not np.isnan(TrI_coeff) and np.isnan(traff_coeff) and np.isnan(traffroadlength_coeff):
+        TrVsubset = df[traffVolhours].copy() * TrV_coeff
+        df[TrafficNO2_nobaseline] = TrVsubset.add(df[traffIntenshours] * TrI_coeff, fill_value=0)
+    elif not np.isnan(TrI_coeff) and np.isnan(TrV_coeff):
         print("only traffic intensity")
-        df[TrafficNO2_nobaseline] = df[traffIntsenshours] * TrI_coeff
+        df[TrafficNO2_nobaseline] = df[traffIntenshours] * TrI_coeff
 
     df[TrafficNO2_nobaseline].fillna(0, inplace=True)
     df[BaselineTrafficNO2] = df[TrafficNO2_nobaseline] + df['baseline_NO2']
     df[TrafficNO2] = df[BaselineTrafficNO2]
     df.loc[df['ON_ROAD'] == 0, TrafficNO2] = 0
-    df_final = df.loc[:,["baseline_NO2","ON_ROAD","road_length", TrafficNO2_nobaseline, TrafficNO2, BaselineTrafficNO2]]
+    df_final = df.loc[:,[id, "baseline_NO2","ON_ROAD", TrafficNO2_nobaseline, TrafficNO2, BaselineTrafficNO2]]
     print(df_final.head(20))
     df_final.to_csv(f"Pred_{suffix}.csv", index=False)
     return df_final
@@ -123,7 +121,7 @@ def create_Eval_df(grid_sp, gridID, point_observations, observationsID, observat
     Returns:
         pd.DataFrame: DataFrame (Eval_df) with the observations joined to the grid.
     """
-    gridjoin = gpd.tools.sjoin(point_observations[[observationsID]+observations_colnames + ["geometry"]], grid_sp, how="inner", op='intersects')
+    gridjoin = gpd.tools.sjoin(point_observations[[observationsID]+observations_colnames + ["geometry"]], grid_sp, how="inner", predicate='intersects')
     gridjoin = gridjoin.dropna(subset=['index_right'])
     gridjoin[gridID] = gridjoin["index_right"]
     gridjoin = gridjoin[[observationsID, gridID]+observations_colnames]
--- ExampleApplications/01DataPreparationExample.py
+++ ExampleApplications/01DataPreparationExample.py
@@ -14,52 +14,70 @@ cellsize = "50m"
 suffix = "TrV_TrI_noTrA"
 
 # Read raster data and DataFrames
-AirPollGrid = gpd.read_file(f"AirPollDeterm_grid{cellsize}.shp")
-PalmesData = gpd.read_file(dataFolder + "/Air Pollution Determinants/PalmesOffRoadMeasurements/Palmesdata2019gridjoin.shp")
-PalmesID = "Code"
-PalmesCols = ["Janry","Fbrry","March","April","May","June","July","Augst","Sptmb","Octbr","Nvmbr","Dcmbr"]
-Palmesdesiredcols =["palmesNO2_1","palmesNO2_2","palmesNO2_3", 
-               "palmesNO2_4","palmesNO2_5","palmesNO2_6","palmesNO2_7",
-               "palmesNO2_8","palmesNO2_9","palmesNO2_10", "palmesNO2_11",
-               "palmesNO2_12"]
-RMSData = gpd.read_file(dataFolder + "/Air Pollution Determinants/RIVMhourlyOffRoadData/RIVMmonthlyhourly.shp")
-RMSID = "SttnC"
-RMShourly = ["MeanNO2_0_1","MeanNO2_1_2","MeanNO2_2_3","MeanNO2_3_4", 
-                          "MeanNO2_4_5","MeanNO2_5_6","MeanNO2_6_7","MeanNO2_7_8",
-                          "MeanNO2_8_9","MeanNO2_9_10","MeanNO2_10_11", "MeanNO2_11_12",
-                          "MeanNO2_12_13","MeanNO2_13_14", "MeanNO2_14_15", "MeanNO2_15_16",
-                          "MeanNO2_16_17","MeanNO2_17_18", "MeanNO2_18_19", 
-                          "MeanNO2_19_20","MeanNO2_20_21","MeanNO2_21_22", 
-                          "MeanNO2_22_23", "MeanNO2_23_24"]
-RMSmonthly = [f"{rivm}_month_{month}"  for month in range(1, 13) for rivm in RMShourly]
-print(RMSData.columns)
-RMSData.columns = list(RMSData.columns[:4]) + RMSmonthly + ["geometry"]
+# AirPollGrid = gpd.read_file(f"AirPollDeterm_grid{cellsize}.shp")
+# PalmesData = gpd.read_file(dataFolder + "/Air Pollution Determinants/PalmesOffRoadMeasurements/Palmesdata2019gridjoin.shp")
+# PalmesID = "Code"
+# PalmesCols = ["Janry","Fbrry","March","April","May","June","July","Augst","Sptmb","Octbr","Nvmbr","Dcmbr"]
+# Palmesdesiredcols =["palmesNO2_1","palmesNO2_2","palmesNO2_3", 
+#                "palmesNO2_4","palmesNO2_5","palmesNO2_6","palmesNO2_7",
+#                "palmesNO2_8","palmesNO2_9","palmesNO2_10", "palmesNO2_11",
+#                "palmesNO2_12"]
+# RMSData = gpd.read_file(dataFolder + "/Air Pollution Determinants/RIVMhourlyOffRoadData/RIVMmonthlyhourly.shp")
+# RMSID = "SttnC"
+# RMShourly = ["MeanNO2_0_1","MeanNO2_1_2","MeanNO2_2_3","MeanNO2_3_4", 
+#                           "MeanNO2_4_5","MeanNO2_5_6","MeanNO2_6_7","MeanNO2_7_8",
+#                           "MeanNO2_8_9","MeanNO2_9_10","MeanNO2_10_11", "MeanNO2_11_12",
+#                           "MeanNO2_12_13","MeanNO2_13_14", "MeanNO2_14_15", "MeanNO2_15_16",
+#                           "MeanNO2_16_17","MeanNO2_17_18", "MeanNO2_18_19", 
+#                           "MeanNO2_19_20","MeanNO2_20_21","MeanNO2_21_22", 
+#                           "MeanNO2_22_23", "MeanNO2_23_24"]
+# RMSmonthly = [f"{rivm}_month_{month}"  for month in range(1, 13) for rivm in RMShourly]
+# print(RMSData.columns)
+# RMSData.columns = list(RMSData.columns[:4]) + RMSmonthly + ["geometry"]
 
 
-Eval_df_Palmes = create_Eval_df(grid_sp = AirPollGrid,
-               gridID = "int_id", 
-               point_observations = PalmesData, 
-               observationsID = PalmesID, 
-               observations_colnames = PalmesCols, 
-               desired_observation_colnames = Palmesdesiredcols, suffix="Palmes")
+# Eval_df_Palmes = create_Eval_df(grid_sp = AirPollGrid,
+#                gridID = "int_id", 
+#                point_observations = PalmesData, 
+#                observationsID = PalmesID, 
+#                observations_colnames = PalmesCols, 
+#                desired_observation_colnames = Palmesdesiredcols, suffix="Palmes")
 
-Eval_df_RMS = create_Eval_df(grid_sp = AirPollGrid,
-                gridID = "int_id", 
-                point_observations = RMSData, 
-                observationsID = RMSID, 
-                observations_colnames = RMSmonthly, 
-                desired_observation_colnames = RMSmonthly, suffix="RMS")
+# Eval_df_RMS = create_Eval_df(grid_sp = AirPollGrid,
+#                 gridID = "int_id", 
+#                 point_observations = RMSData, 
+#                 observationsID = RMSID, 
+#                 observations_colnames = RMSmonthly, 
+#                 desired_observation_colnames = RMSmonthly, suffix="RMS")
 
 # # merge the two dataframes
 # Eval_df = Eval_df_Palmes.merge(Eval_df_RMS, on="int_id", how="inner")
-# Eval_df.to_csv(f"Eval_{cellsize}{suffix}test.csv", index=False)
+# Eval_df.to_csv(f"Eval_{cellsize}.csv", index=False)
 
 
-# Pred_df = pd.read_csv(f"Pred_{cellsize}{suffix}.csv")               # Read Pred DataFrame
-# create_traffic_emission_columns(df, traff_coeff=np.nan, TrI_coeff=np.nan, traffroadlength_coeff=np.nan, suffix="")
+### make Pred_df
+# read csv
+grid_dataframe = pd.read_csv(f"AirPollDeterm_grid{cellsize}_Predictors_full.csv")
+# this dataframe has all predictor variables aggregated to the grid level, including the land use predictors and traffic volume and intensity
+# the traffic volume and intensity are in the columns "TrV0_1","TrV1_2",.. and "TrI0_1","TrI1_2", ... respectively
 
-# create_Eval_df(grid_sp, gridID, point_observations, observationsID, observations_colnames, desired_observation_colnames = None, suffix = ""):
+# create columns for NO2 with TrafficVolume and Traffic Intensity without transport area
+if cellsize == "50m":
+    grid_dataframe["baseline_NO2"] = (1.902e+01 + (grid_dataframe["INDUS_5000"] * 1.939e-07)
+                                    + (grid_dataframe["WATER_1000"] * 7.695e-08) + (grid_dataframe["AGRI_5000"] * -2.195e-07)
+                                    + (grid_dataframe["PORT_5000"] * 1.531e-07) + (grid_dataframe["NATUR_5000"] * 6.079e-07)
+                                    + (grid_dataframe["INDUS_100"] * 6.005e-05) + (grid_dataframe["GreenCover100"] * -6.919e-01))
+    grid_dataframe.loc[grid_dataframe["baseline_NO2"] < 0, "baseline_NO2"] = 0
+    grid_dataframe = create_traffic_emission_columns(df = grid_dataframe, id = "int_id", TrV_coeff =  2.845e-02 , TrI_coeff = 1.072e-04, suffix="TrV_TrI_noTrAxxx")
 
+elif cellsize == "25m":
+    grid_dataframe["baseline_NO2"] = (-5.440e+00 + (grid_dataframe["POP_5000"] * 5.358e-06) +
+                                    (grid_dataframe["INDUS_5000"] * 5.499e-07) + (grid_dataframe["NATUR_5000"] * 1.150e-06) +
+                                    (grid_dataframe["AGRI_5000"] * 1.170e-07) + (grid_dataframe["PORT_5000"] * 2.297e-07) +
+                                    (grid_dataframe["RES_5000"] * 1.823e-07))
+    grid_dataframe.loc[grid_dataframe["baseline_NO2"] < 0, "baseline_NO2"] = 0
+    grid_dataframe = create_traffic_emission_columns(df = grid_dataframe, id = "int_id", TrV_coeff = 1.586e-02 , TrI_coeff = 1.306e-04, suffix="TrV_TrI_noTrAxxx")
+    
 
 # Pred_df = pd.read_csv(f"Pred_{cellsize}{suffix}.csv")               # Read Pred DataFrame
 # airpoll_grid_raster = xr.open_dataarray(f"AirPollDeterm_grid_{cellsize}.tif", engine="rasterio")[0] # Read raster data using rasterio
